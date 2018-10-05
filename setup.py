8c9b72fab26bf20166f4e7d3636904689f147add
Germey <cqc@cuiqingcai.com>
2018-10-05 22:57:52 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from os.path import join, isfile
 from os import walk
-import model_zoo
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
 
 
 def read_file(filename):
@@ -12,6 +18,33 @@ def read_file(filename):
 def read_requirements(filename):
     return [line.strip() for line in read_file(filename).splitlines()
             if not line.startswith('#')]
+
+
+NAME = 'model-zoo'
+FOLDER = 'model_zoo'
+DESCRIPTION = 'A framework to help you build model much more easily.'
+URL = 'https://github.com/ModelZoo/ModelZoo'
+EMAIL = 'cqc@cuiqingcai.com'
+AUTHOR = 'Germey'
+REQUIRES_PYTHON = '>=3.4.0'
+VERSION = None
+
+REQUIRED = read_requirements('requirements.txt')
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, FOLDER, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 
 def package_files(directories):
@@ -26,27 +59,75 @@ def package_files(directories):
     return paths
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+    
+    description = 'Build and publish the package.'
+    user_options = []
+    
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+    
+    def initialize_options(self):
+        pass
+    
+    def finalize_options(self):
+        pass
+    
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+        
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+        
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='model-zoo',
-    version=model_zoo.version(),
-    description='A framework to help you build model much more easily',
-    keywords=['model_zoo', 'tensorflow', 'keras'],
-    author='germey',
-    author_email='cqc@cuiqingcai.com',
-    url='http://pypi.python.org/pypi/model-zoo/',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
     license='MIT',
-    install_requires=read_requirements('requirements.txt'),
-    packages=find_packages(),
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
     package_data={
         '': package_files([
-            'model_zoo/VERSION'
+            'requirements.txt',
         ])
     },
-    publish=[
-        'sudo python3 setup.py bdist_egg',
-        'sudo python3 setup.py sdist',
-        'sudo python3 setup.py bdist_egg upload',
-        'sudo python3 setup.py sdist upload',
-        'twine upload dist/* '
-    ]
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
